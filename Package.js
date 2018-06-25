fd7978cde1cd35f20c940c00fbf106b018d86c95
Coussy <40577094+Coussy@users.noreply.github.com>
2018-06-25 19:15:14 +0100

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,15 @@
+{
+  "name": "spain",
+  "version": "1.0.0",
+  "description": "aph spain bot",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "cous",
+  "license": "ISC",
+  "dependencies": {
+    "discord.io": "https://github.com/woor/discord.io/tarball/gateway_v6",
+    "discord.js": "^11.3.2"
+  }
+}
