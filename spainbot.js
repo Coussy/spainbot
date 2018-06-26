31675cfdfa3596590ab434ecff4ac31a0b9b2140
Coussy <40577094+Coussy@users.noreply.github.com>
2018-06-26 14:31:14 +0100

    Update spainbot.js

--- spainbot.js
+++ spainbot.js
@@ -3,28 +3,66 @@ const Discord = require("discord.js");
 
 const bot = new Discord.Client({disableEveryone: true});
 
-bot.on("ready", async () => {
+bot.on("ready",() => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setGame("Romano's sausage");
+  bot.user.setActivity("Romano's sausage");
 });
 
 var http = require("http");
 setInterval(function() {
    http.get("http://spainbot.herokuapp.com");
 }, 900000);
-  
-bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
 
-  let messageArray =message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
+bot.on('message', message =>{
+  if (message.content.toLowerCase().includes ('porty')) {
+    var choices = ['fish-breath','mom still likes me better','towel gangster'];
+    var response = Math.floor(Math.random() * choices.length);
+    message.channel.send(choices[response]);
+  } else
 
-  if(cmd === `hello`){
-    return message.channel.send("Hola!");
+  if (message.content.toLowerCase().includes ('fake!quote!')) {
+    var choices = ['too bad England’s not here to enjoy England not being here.','romano fill my files with your data!!!','I have a demon in me but we are friends :)','gay','crocs are comfy','h','romano is my fursona'];
+    var response = Math.floor(Math.random() * choices.length);
+    message.channel.send(choices[response]);
+  } else
+
+  if (message.content.toLowerCase().includes ('validate me')) {
+    var choices = ['you are 100% valid! :sparkles:','you are doing great!!!','Boss is so proud of you!','currently sending you a virtual hug'];
+    var response = Math.floor(Math.random() * choices.length);
+    message.channel.send(choices[response]);
   }
-
+  
 });
 
-bot.login(NDU4Njg4NTU1NDE4NjQ4NTc2.DhGZAg.fjssVVGl8FMi0RKVXthup1_YvjE);
+bot.on('message', message =>{
+  if (message.content.toLowerCase().includes ("hi spainbot")) {
+    message.channel.send("¡Hola!")
+  } else
+
+  if (message.content.toLowerCase().includes ("romabot")) {
+    message.channel.send("you mispelled perfection :heart:")
+  } else
+
+  if (message.content.toLowerCase().includes ("amore")) {
+    message.channel.send(":eyes: :heart:")
+  } else
+
+  if (message.content.toLowerCase().includes ("ily spainbot")) {
+    message.channel.send("I LOVE YOU MORE!!!!! :two_hearts:")
+  } else
+
+  if (message.content.toLowerCase().includes ("olive oil")) {
+    message.channel.send(":peanuts: :peanuts: :peanuts:")
+  } else
+
+  if (message.content.toLowerCase().includes ("hewwo")) {
+    message.channel.send("uwu nya")
+  } else
+
+  if (message.content.toLowerCase().includes ("alfie bot")) {
+    message.channel.send("that guy has knees of steel...")
+  }
+
+})
+
+bot.login(NDU4Njg4NTU1NDE4NjQ4NTc2.DhMhbg.0oJRSVQATQpfVle5002QSSZAokM);
